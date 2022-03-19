d038fd2aa73c7e25fd81997b6cbdb902d2880606
Szymon <wojdyla.szymon@gmail.com>
2022-03-19 13:05:04 +0000

    models updated

---
+++ models/animal.py
@@ -0,0 +1,8 @@
+class Animal:
+    def __init__(self, name, dob, type, contact, notes, id = None):
+        self.name = name
+        self.dob = dob
+        self.type = type
+        self.contact = contact
+        self.notes = notes
+        self.id = id
---
+++ models/vet.py
@@ -0,0 +1,6 @@
+class Vet:
+    def __init__(self, name, contact, status, id = None):
+        self.name = name
+        self.contact = contact
+        self.status = status
+        self.id = id
