28f0095add1c844b1c0f2d68cb26cd6b860294b4
Szymon <wojdyla.szymon@gmail.com>
2022-03-20 13:27:53 +0000

    vet_id atribute added to Animal model class and tested

--- models/animal.py
+++ models/animal.py
@@ -1,8 +1,9 @@
 class Animal:
-    def __init__(self, name, dob, type, contact, notes, id = None):
+    def __init__(self, name, dob, type, contact, notes, vet_id, id = None):
         self.name = name
         self.dob = dob
         self.type = type
         self.contact = contact
         self.notes = notes
+        self.vet_id = vet_id
         self.id = id
--- tests/animal_test.py
+++ tests/animal_test.py
@@ -5,7 +5,7 @@ from models.animal import *
 class AnimalTest(unittest.TestCase):
 
     def setUp(self):
-        self.animal = Animal("Rex", "03-05-2021", "pudel", "07911-123456", "Regular check-up")
+        self.animal = Animal("Rex", "03-05-2021", "pudel", "07911-123456", "Regular check-up", 1)
         
     def test_animal_has_name(self):
         self.assertEqual("Rex", self.animal.name)
@@ -17,3 +17,5 @@ class AnimalTest(unittest.TestCase):
         self.assertEqual("07911-123456", self.animal.contact)
     def test_animal_has_notes(self):
         self.assertEqual("Regular check-up", self.animal.notes)
+    def test_animal_has_vet_id(self):
+        self.assertEqual(1, self.animal.vet_id)
