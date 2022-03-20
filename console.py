61132d09122c9311bb5a876e8680d095efe8ecbb
Szymon <wojdyla.szymon@gmail.com>
2022-03-20 11:40:14 +0000

    updated - animal - added and tested

--- console.py
+++ console.py
@@ -3,14 +3,16 @@ from models.vet import Vet
 
 from repositories import vet_repository, animal_repository
 
-# animal_1 = Animal("Rex", "03-02-2021", "pudel", "07911-123456", "regular check-up")
-# animal_repository.save(animal_1)
+animal_1 = Animal("Meg", "03-02-2021", "pudel", "07911-123456", "regular check-up")
+animal_repository.save(animal_1)
 
 # print(animal_repository.select(animal_1.id).__dict__)
 
+# animal_repository.delete(2)
+animal_1.type = "York"
+animal_repository.update(animal_1)
 
 
-animal_repository.delete(2)
 
 all_animals = animal_repository.select_all()
 for animal in all_animals:
--- repositories/animal_repository.py
+++ repositories/animal_repository.py
@@ -40,3 +40,8 @@ def delete(id):
 def delete_all():
     sql = "DELETE FROM animals"
     run_sql(sql)
+
+def update(animal):
+    sql = "UPDATE animals SET (name, dob, type, contact, notes) = (%s, %s, %s, %s, %s) WHERE id = %s"
+    values = [animal.name, animal.dob, animal.type, animal.contact, animal.notes, animal.id]
+    run_sql(sql, values)
