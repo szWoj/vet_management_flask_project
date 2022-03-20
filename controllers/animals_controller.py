b8631aff47b39dbaba2c7eff3258455e01e0c7f7
Szymon <wojdyla.szymon@gmail.com>
2022-03-20 15:32:45 +0000

    all animals displayed using animals/animals_all.html template

--- controllers/animals_controller.py
+++ controllers/animals_controller.py
@@ -8,3 +8,8 @@ animals_blueprint = Blueprint("animals", __name__)
 @animals_blueprint.route('/animals')
 def animals_menu():
     return render_template('animals/index.html')
+
+@animals_blueprint.route('/animals/animals_all')
+def animals_all():
+    animals = animal_repository.select_all()
+    return render_template('animals/all_animals.html', animals=animals)
