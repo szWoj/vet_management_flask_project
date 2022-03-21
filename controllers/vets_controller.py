d7b9eab967c9b1dff25eef2ceb22ec490fa44509
Szymon <wojdyla.szymon@gmail.com>
2022-03-21 15:10:53 +0000

    vet can be updated from browser

--- controllers/vets_controller.py
+++ controllers/vets_controller.py
@@ -45,3 +45,19 @@ def create_vet():
     vet_repository.save(vet)
 
     return redirect('/vets/vets_all')
+
+@vets_blueprint.route("/vets/<id>/edit", methods = ["GET"])
+def edit_vet(id):
+    vet = vet_repository.select(id)
+    
+    return render_template("vets/edit.html", vet = vet)
+
+@vets_blueprint.route("/vets/<id>", methods = ["POST"])
+def update_vet(id):
+    name = request.form["name"]
+    contact = request.form["contact"]
+    status = request.form["status"]
+
+    vet = Vet(name, contact, status, id)
+    vet_repository.update(vet)
+    return redirect('/vets/vets_all')
