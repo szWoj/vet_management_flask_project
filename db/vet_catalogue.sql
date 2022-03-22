3e5bce938b2e5567a8a64647c36b59f93d0cb22a
Szymon <wojdyla.szymon@gmail.com>
2022-03-22 10:56:29 +0000

    extensions start here - appointments table created

--- db/vet_catalogue.sql
+++ db/vet_catalogue.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS appointments;
 DROP TABLE IF EXISTS animals;
 DROP TABLE IF EXISTS vets;
 
@@ -18,3 +19,11 @@ CREATE TABLE animals (
     notes TEXT,
     vet_id INT REFERENCES vets(id)
 );
+
+CREATE TABLE appointments (
+    id SERIAL PRIMARY KEY,
+    date VARCHAR(255),
+    checked_in BOOLEAN,
+    vet_id INT REFERENCES vets(id),
+    animal_id INT REFERENCES animals(id)
+)
