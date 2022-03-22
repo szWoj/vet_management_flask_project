a7963677c363978f31a642aa9c9e73de436bd2c0
Szymon <wojdyla.szymon@gmail.com>
2022-03-22 11:10:30 +0000

    appointment class added

--- db/vet_catalogue.sql
+++ db/vet_catalogue.sql
@@ -23,6 +23,7 @@ CREATE TABLE animals (
 CREATE TABLE appointments (
     id SERIAL PRIMARY KEY,
     date VARCHAR(255),
+    time VARCHAR(255),
     checked_in BOOLEAN,
     vet_id INT REFERENCES vets(id),
     animal_id INT REFERENCES animals(id)
---
+++ models/appointment.py
@@ -0,0 +1,11 @@
+class Appointment:
+    def __init__(self, date, time, vet, animal, checked_in = False id = None):
+        self.date = date
+        self.time = time
+        self.vet = vet
+        self.animal = animal
+        self.checked_in = checked_in
+
+    def mark_checked_in(self):
+        self.checked_in = True
+        
